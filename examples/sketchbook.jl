060542c7aac79e262bd2f6fd9d0ec4055f9edabc
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2023-01-18 10:31:47 +0100

    Before incorporating comments from Halvor and Olav

--- examples/sketchbook.jl
+++ examples/sketchbook.jl
@@ -6,6 +6,12 @@ import JutulDarcy
 struct AdsorptionFlowSystem <: JutulDarcy.JutulSystem
 end
 
+JutulDarcy.number_of_phases(::AdsorptionFlowSystem) = 1
+
+function JutulDarcy.degrees_of_freedom_per_entity(model::Jutul.SimulationModel{G, S}, v::JutulDarcy.TotalMasses) where {G<:Any, S<:AdsorptionFlowSystem}
+    JutulDarcy.number_of_phases(model.system)
+end
+
 
 JutulDarcy.number_of_components(sys::AdsorptionFlowSystem) = 2
 JutulDarcy.has_other_phase(::AdsorptionFlowSystem) = false
@@ -20,12 +26,24 @@ function Jutul.select_primary_variables!(S, ::AdsorptionFlowSystem, model::Jutul
     S[:qN2] = JutulDarcy.TotalMass()
 end
 
+function Jutul.apply_forces_to_equation!(acc, storage, model::SimulationModel{D, S}, eq::ConservationLaw, eq_s, force::V, time) where {V <: AbstractVector{SourceTerm{I, F, T}}, D, S<:MultiPhaseSystem} where {I, F, T}
+    state = storage.state
+    if haskey(state, :RelativePermeabilities)
+        kr = state.RelativePermeabilities
+    else
+        kr = 1.0
+    end
+    mu = state.PhaseViscosities
+    rhoS = reference_densities(model.system)
+    insert_phase_sources!(acc, model, kr, mu, rhoS, force)
+end
+
 function Jutul.select_secondary_variables!(S, ::AdsorptionFlowSystem, model::Jutul.SimulationModel)
     S[:cTot] = JutulDarcy.TotalMass()
     S[:cCO2] = JutulDarcy.TotalMass()
     S[:cN2] = JutulDarcy.TotalMass()
     S[:avm] = JutulDarcy.TotalMass()
-    
+    S[:TotalMasses] = JutulDarcy.TotalMasses()
 end
 
 function Jutul.select_equations!(eqs, sys::AdsorptionFlowSystem, model::Jutul.SimulationModel)
@@ -58,3 +76,5 @@ sys = AdsorptionFlowSystem()
 model = Jutul.SimulationModel(G, sys)
 @show model
 end
+
+Mocca.model
