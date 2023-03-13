ede2750b7a7528ecbf771916193f7d36369140f0
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2023-03-13 16:48:54 +0100

    Added plot wrt to time.

--- src/plot.jl
+++ src/plot.jl
@@ -41,3 +41,37 @@ function plot_states(states)
         return f
     end
 end
+
+function plot_outlet(states)
+    return MakiePublication.with_theme(MakiePublication.theme_web()) do
+        f = CairoMakie.Figure()
+        nc = size(states[end][:Pressure], 1)
+        x = collect(LinRange(0.0, 1.0, nc))
+        key_to_label = Dict(
+            :y => "y",
+            :Pressure => "p",
+            :adsorptionRates => "q",
+            :Temperature => "T",
+            :WallTemperature => "T_{wall}"
+        )
+
+        for (nsymb, symbol) in enumerate([:y, :Pressure, :adsorptionRates, :Temperature, :WallTemperature])
+            @show symbol
+            # Truncating to float16 seems to be needed due to some weird cairomakie bug:
+            # https://discourse.julialang.org/t/range-step-cannot-be-zero/66948/10
+            # TODO: Fix the above
+            if size(states[end][symbol], 2) == 1
+                ax = CairoMakie.Axis(f[nsymb, 1], title=String(symbol), xlabel=CairoMakie.L"x", ylabel=CairoMakie.L"%$(key_to_label[symbol])")
+                CairoMakie.lines!(ax, Float16.([blah[symbol][end] for blah in states]), color=:darkgray)
+            else
+                for i in 1:size(states[end][symbol], 1)
+                    ax = CairoMakie.Axis(f[nsymb, i], title=String(symbol), xlabel=CairoMakie.L"x", ylabel=CairoMakie.L"%$(key_to_label[symbol])_%$i")
+                    
+                    CairoMakie.lines!(ax, Float16.([blah[symbol][i, end] for blah in states]), color=:darkgray)
+                end
+            end
+        end
+        CairoMakie.resize!(f.scene, (2 * 400, 3 * 400))
+        return f
+    end
+end
