4efe8d71d057777645a4b96959ea86e6418ac3d2
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2023-05-13 08:43:50 +0200

    BC now per component

--- src/bc.jl
+++ src/bc.jl
@@ -1,5 +1,16 @@
+struct PressurationBC
 
-function JutulDarcy.apply_flow_bc!(acc, q, bc, model::Jutul.SimulationModel{<:Any,T}, state, time) where {T<:AdsorptionFlowSystem}
+
+end
+
+function JutulDarcy.apply_flow_bc!(
+    acc,
+    q,
+    bc,
+    model::Jutul.SimulationModel{<:Any,T},
+    state,
+    time,
+) where {T<:AdsorptionFlowSystem}
     sys = model.system
     mu = sys.p.fluid_viscosity
     concentrations = state.concentrations
@@ -26,7 +37,37 @@ function JutulDarcy.apply_flow_bc!(acc, q, bc, model::Jutul.SimulationModel{<:An
 end
 
 
-function Jutul.apply_forces_to_equation!(acc, storage, model::AdsorptionFlowModel, eq::Jutul.ConservationLaw{:ColumnConservedEnergy}, eq_s, force, time)
+function Jutul.apply_forces_to_equation!(
+    acc,
+    storage,
+    model::AdsorptionFlowModel,
+    eq::Jutul.ConservationLaw{:adsorptionRates},
+    eq_s,
+    force::PressurationBC,
+    time,
+)
+    @info "In adsorption bc"
+    # state = storage.state
+    # p = state.Pressure
+    # for bc in force
+    #     c = bc.cell
+    #     T_f = bc.trans_flow
+    #     Δp = p[c] - bc.pressure
+    #     q = T_f*Δp
+    #     acc_i = view(acc, :, c)
+    #     apply_flow_bc!(acc_i, q, bc, model, state, time)
+    # end
+end
+
+function Jutul.apply_forces_to_equation!(
+    acc,
+    storage,
+    model::AdsorptionFlowModel,
+    eq::Jutul.ConservationLaw{:ColumnConservedEnergy},
+    eq_s,
+    force::PressurationBC,
+    time,
+)
     @info "In column bc"
     # state = storage.state
     # p = state.Pressure
@@ -40,7 +81,15 @@ function Jutul.apply_forces_to_equation!(acc, storage, model::AdsorptionFlowMode
     # end
 end
 
-function Jutul.apply_forces_to_equation!(acc, storage, model::AdsorptionFlowModel, eq::Jutul.ConservationLaw{:WallConservedEnergy}, eq_s, force, time)
+function Jutul.apply_forces_to_equation!(
+    acc,
+    storage,
+    model::AdsorptionFlowModel,
+    eq::Jutul.ConservationLaw{:WallConservedEnergy},
+    eq_s,
+    force::PressurationBC,
+    time,
+)
     @info "In wall bc"
     # state = storage.state
     # p = state.Pressure
