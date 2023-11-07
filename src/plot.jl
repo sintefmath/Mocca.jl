f5a5ab3117447aea3af2635f54606b21e5292f36
Francesca Watson <francesca.watson@sintef.no>
2023-11-07 09:20:40 +0100

    Update titles for plotting

--- src/plot.jl
+++ src/plot.jl
@@ -12,13 +12,25 @@ function units_dict()
     return unit_label
 end
 
+function prettyVarNames(vars::Vector{Symbol})
+    pretty_names = Dict()
+
+    for (i, symb) in enumerate(vars)
+        var = String(symb)
+        words = split(var, r"(?=[A-Z])")
+        pretty_names[symb] = join(words, " ")
+    end
+
+    return pretty_names
+end
+
 
 function plot_cell(states, model, timesteps, cell)
 
     pvars = model.primary_variables.keys
     comp_names = model.system.component_names
 
-
+    pretty_names = prettyVarNames(pvars)
     units = units_dict()
     t = Float64.(cumsum(timesteps))
 
@@ -26,22 +38,22 @@ function plot_cell(states, model, timesteps, cell)
     ga = f[2,3] = GridLayout()
     r = 1
 
-    for (i, symbol) in enumerate(pvars)
+    for (i, symb) in enumerate(pvars)
         c = i
         if i > 3
             c = i - 3
             r = 2
         end
         ax = Axis(f[r,c],
-            title=String(symbol),
+            title=pretty_names[symb],
             xlabel=L"t\; [s]",
-            ylabel=L"%$(units[symbol])")
+            ylabel=L"%$(units[symb])")
 
-        if size(states[end][symbol], 2) == 1
-           lines!(ax, t, [result[symbol][cell] for result in states])
+        if size(states[end][symb], 2) == 1
+           lines!(ax, t, [result[symb][cell] for result in states])
         else
-            for k in 1:size(states[end][symbol], 1)
-                lines!(ax, t, [result[symbol][k, cell] for result in states], label = comp_names[k])
+            for k in 1:size(states[end][symb], 1)
+                lines!(ax, t, [result[symb][k, cell] for result in states], label = comp_names[k])
             end
             leg = Legend(f[2,3], ax, tellwidth=false)
             
@@ -66,22 +78,22 @@ function plot_state(state, model)
     ga = f[2,3] = GridLayout()
     r = 1
 
-    for (i, symbol) in enumerate(pvars)
+    for (i, symb) in enumerate(pvars)
         c = i
         if i > 3
             c = i - 3
             r = 2
         end
         ax = Axis(f[r,c],
-            title=String(symbol),
+            title=String(symb),
             xlabel=L"x\; [m]",
-            ylabel=L"%$(units[symbol])")
+            ylabel=L"%$(units[symb])")
 
-        if size(state[symbol], 2) == 1
-           lines!(ax, x, state[symbol])
+        if size(state[symb], 2) == 1
+           lines!(ax, x, state[symb])
         else
-            for k in 1:size(state[symbol], 1)
-                lines!(ax, x, state[symbol][k,:], label = comp_names[k])
+            for k in 1:size(state[symb], 1)
+                lines!(ax, x, state[symb][k,:], label = comp_names[k])
             end
             leg = Legend(f[2,3], ax, tellwidth=false)
             
