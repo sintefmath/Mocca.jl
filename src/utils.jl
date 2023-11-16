136bff0ba164e286cf2083eda7b9acf90d49af99
Olav Møyner <olav.moyner@gmail.com>
2023-11-16 19:35:17 +0100

    Fix definition of dx

--- src/utils.jl
+++ src/utils.jl
@@ -2,14 +2,19 @@ export mocca_domain
 """
     mocca_domain(mesh::Jutul.CartesianMesh, system::AdsorptionSystem)
 
-Set up a data domain for carbon capture simulation. Currently only implemented
-for a CartesianMesh instance.
+Set up a data domain for carbon capture simulation.
 """
-function mocca_domain(mesh::Jutul.CartesianMesh, system::AdsorptionSystem; kwarg...)
+function mocca_domain(mesh, system::AdsorptionSystem; kwarg...)
     domain = JutulDarcy.reservoir_domain(mesh, porosity = system.p.Φ, permeability = system.permeability)
     domain[:diffusion_coefficient] = system.dispersion
-    domain[:thermal_conductivity] = system.p.K_z 
-    domain[:dx] = mesh.dims[2]
+    domain[:thermal_conductivity] = system.p.K_z
+    nc = Jutul.number_of_cells(mesh)
+    dx = zeros(nc)
+    for i in 1:nc
+        dx[i] = first(Jutul.cell_dims(mesh, i))
+    end
+    domain[:dx] = dx
+
     for (k, v) in kwarg
         domain[k] = v
     end
