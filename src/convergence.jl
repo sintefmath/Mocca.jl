0f5c3dbb8aa7475a278adf2c0f9319d7484fcaed
Olav Møyner <olav.moyner@gmail.com>
2023-11-16 12:36:16 +0100

    Catch up to Jutul changes

--- src/convergence.jl
+++ src/convergence.jl
@@ -1,6 +1,6 @@
 using LinearAlgebra
 using Tullio
-function Jutul.convergence_criterion(model::Jutul.SimulationModel{D,S}, storage, eq::Jutul.ConservationLaw, eq_s, r; dt=1) where {D,S<:AdsorptionSystem}
+function Jutul.convergence_criterion(model::Jutul.SimulationModel{D,S}, storage, eq::Jutul.ConservationLaw, eq_s, r; dt=1.0, update_report = missing) where {D,S<:AdsorptionSystem}
     n = Jutul.number_of_equations_per_entity(model, eq)
     @tullio max e[i] := abs(r[i, j])
     if n == 1
