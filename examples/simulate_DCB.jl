46c78feb1786e3b8a5a4f775bafcb733a2f82c3b
Andreas Brostrøm <andreas.brostrom@sintef.no>
2025-06-12 13:04:38 +0200

    Clean up example scripts
    Make sure that the generated docs are properly formatted and function
    outputs only showed when it should

--- examples/simulate_DCB.jl
+++ examples/simulate_DCB.jl
@@ -1,12 +1,12 @@
 # # Direct Column Breakthrough simulation
 
-# This example shows how to setup and run a direct column breakthrough simulation
-# as described in [Haghpanah et al. 2013](http://dx.doi.org/10.1021/ie302658y)
+# This example shows how to set up and run a direct column breakthrough (DCB) simulation
+# as described in [Haghpanah et al. 2013](http://dx.doi.org/10.1021/ie302658y).
 # This simulation inolves injection of a two component flue gas (CO2 and N2)
-# into a column of Zeolite 13X initially filled with N2. 
+# into a column of Zeolite 13X initially filled with N2.
 
 # Adsorption onto Zeolite 13X is modelled with a dual-site Langumuir adsorption isotherm.
-# Injection flow rate is fixed at the rhs of the column and the lhs of the column
+# Injection flow rate is fixed at the right hand side of the column and the left hand side of the column
 # is open.
 # There is no heat transfer between the column and the column wall.
 #-
@@ -17,9 +17,9 @@ import Mocca
 
 #=
 Then we define parameters which we want. We have defined a structure containing
-parameters from Haghpanah et al. 2013 which we load now. 
-As we are doing a DCB simulation we will set the heat transfer coefficient between 
-the column and the wall and the wall and the outside to 0.
+parameters from Haghpanah et al. 2013 which we load now.
+As we are doing a DCB simulation we will set the heat transfer coefficient between
+the column and the wall and the wall and the outside to zero.
 =#
 
 constants = Mocca.HaghpanahConstants{Float64}(h_in=0.0,h_out=0.0);
@@ -29,50 +29,48 @@ constants = Mocca.HaghpanahConstants{Float64}(h_in=0.0,h_out=0.0);
 # the domain (grid) over which we will solve the equations and information
 # about the system of equations which we are solving.
 #-
-# Because JutulDarcy has its roots in reservoir simulation we formulate 
+# Because JutulDarcy has its roots in reservoir simulation we formulate
 # our velocity equation in the following form:
 #
 # ```math
-# q = -\frac{k}{\mu}\frac{\partial{P}\partial{x}}
+# v = -\frac{k}{\mu}(\nabla P)
 # ```
 #
-# where `k` is known as the permeability. 
-# In this instance we use the pressure drop equation for plug flow in a packed 
+# where `k` is known as the permeability.
+# In this instance we use the pressure drop equation for plug flow in a packed
 # bed:
 #
 # ```math
-# v=-\frac{4}{150}\left(\frac{\epsilon}{1-\epsilon}\right)^2 r_{i n}^2 \frac{1}{\mu}(\nabla P)
+# v=-\frac{4}{150}\frac{\epsilon^3}{(1-\epsilon)^2} r_{i n}^2 \frac{1}{\mu}(\nabla P)
 # ```
 #
 # The permeability of the system is then given by:
 #
 # ```math
-# k = \frac{4}{150}\left(\frac{\epsilon}{1-\epsilon}\right)^2 r_{i n}^2
+# k = \frac{4}{150}\frac{\epsilon^3}{(1-\epsilon)^2} r_{i n}^2
 # ```
-# We add the parameters and the desired permeability as an input.
+# We calculate the permability and dispersion which are used to specify the model
 permeability = Mocca.compute_permeability(constants);
 axial_dispersion = Mocca.calc_dispersion(constants);
 
-# We setup a two component adsorption system. This system type is associated 
-# with the appropriate equations and primary and secondary variables. 
+# We set up a two component adsorption system. This system type is associated
+# with the appropriate equations and primary and secondary variables.
 
 system = Mocca.TwoComponentAdsorptionSystem(; permeability = permeability, dispersion = axial_dispersion, p = constants);
 
 # Jutul uses finite volume discretisation in space. To model a 1D cylindrical column
-# we setup a cartesian grid with ncells x 1 x 1 dimensions.
-# To ensure we have the correct interface area between cells we set each dimension 
-# to the square root of the inner column area.
+# we setup a cartesian grid with dimensions ncells x 1 x 1.
+# To ensure we have the correct interface area between cells we set each dimension
+# to the square root of the inner column area. We can then define the simulation domain.
 
 ncells = 200;
 dx = sqrt(pi*constants.r_in^2);
 mesh = Jutul.CartesianMesh((ncells, 1, 1), (constants.L, dx, dx));
-
-
-# The domain also contains the mass diffusion coefficient to calculate mass 
-# transport between cells and the thermal conductivity to calculate heat 
-# transfer.
-
 domain = Mocca.mocca_domain(mesh, system);
+
+# The domain also contains the mass diffusion coefficient to calculate mass
+# transport between cells and the thermal conductivity to calculate heat
+# transfer.
 
 # # Create the model
 # Now we can assemble the model which contains the domain and the system of equations.
@@ -80,80 +78,80 @@ model = Jutul.SimulationModel(domain, system, general_ad = true);
 
 # # Setup the initial state
 
-# The final thing required to create the simulator is the intial state of the 
-# system. 
+# The final thing required to create the simulator is the intial state of the
+# system.
 
 bar = Jutul.si_unit(:bar);
 P_init = 1*bar;
 T_init = 298.15;
 Tw_init = constants.T_a;
 
-# To avoid numerical errors we set the initial CO2 concentration to be very 
+# To avoid numerical errors we set the initial CO2 concentration to be very
 # small instead of 0.
 
 yCO2 = fill(1e-10, ncells)
 y_init = hcat(yCO2, 1 .- yCO2);
 
+# Now we can initialise the state in the column
 state0, prm = Mocca.initialise_state_AdsorptionColumn(P_init, T_init, Tw_init, y_init, model);
 
-# # Setup the timestepping and boundary conditions 
+# # Setup the timestepping and boundary conditions
 
-# For the DCB we are only running the adsorption stage of a VSA process. 
-# We will use a total time of 5000 seconds with 1 second timesteps.
+# For the DCB we are only running the adsorption stage of a VSA process.
+# We will use a total time of 5000 seconds with a single report step
 
 t_ads = 5000;
 maxdt = 5000.0;
-
-
 numsteps = Int(floor(t_ads / maxdt));
 timesteps = fill(maxdt, numsteps);
 
-# We setup boundary conditions for an adsorption stage. AdsorptionBC sets a fixed 
-# velocity and concentration and temperature at the inlet and fixed pressure at
-# the outlet. By convention we assume the inlet bc is applied on the left hand 
-# side and the outlet bc is applied on the right hand side. 
+# We set up boundary conditions for an adsorption stage. AdsorptionBC sets a fixed
+# velocity, concentration and temperature at the inlet, and fixed pressure at
+# the outlet. By convention we assume the inlet bc is applied on the left hand
+# side and the outlet bc is applied on the right hand side.
 
 bc = Mocca.AdsorptionBC(y_feed = constants.y_feed, PH = constants.p_high, v_feed = constants.v_feed,
                                 T_feed = constants.T_feed, cell_left = 1, cell_right = ncells);
 
-
-sim_forces = Jutul.setup_forces(model,bc=bc);
+sim_forces = Jutul.setup_forces(model, bc=bc);
 
 # # Simulate
-# Now we are ready to run the simulation. For more verbose simulation output 
-# info_level can be set to a number higher than 0.
 
-# Set up timesteppers based on target changes
+# Set up timesteppers based on target changes with an initial timestep of 1 day
 t_c = Jutul.VariableChangeTimestepSelector(:y, 0.01, relative = false)
 t_t = Jutul.VariableChangeTimestepSelector(:Temperature, 10.0, relative = false)
-t_p = Jutul.VariableChangeTimestepSelector(:Pressure, 10.0, relative = false)
-# Initial timestep of 1 day
+t_p = Jutul.VariableChangeTimestepSelector(:Pressure, 10.0, relative = false);
 t_base = Jutul.TimestepSelector(initial_absolute = 1.0)
-timesteppers = [t_base, t_c, t_t, t_p]
+timesteppers = [t_base, t_c, t_t, t_p];
 
-sim = Jutul.Simulator(model, state0 = state0, parameters = prm)
+# We define the simulation setup with initial states and parameters, a linear solver
+# and other configurable options
+sim = Jutul.Simulator(model; state0 = state0, parameters = prm)
 
 lsolve = Jutul.LUSolver()
-# lsolve = Jutul.GenericKrylov(preconditioner = Jutul.ILUZeroPreconditioner(), rtol = 1e-4)
 
-cfg = Jutul.simulator_config(sim,
+cfg = Jutul.simulator_config(sim;
     timestep_selectors = timesteppers,
     output_substates = true,
     linear_solver = lsolve,
     info_level = 0
-)
+);
 
-result = Jutul.simulate!(sim, timesteps,
+# For more verbose simulation output
+# info_level can be set to a number higher than 0.
+
+# Now we are ready to run the simulation.
+result = Jutul.simulate!(sim, timesteps;
     config = cfg,
     forces = sim_forces,
-)
+);
 # # Plot
-# Get the substates and subtimesteps used inside the simulator
+# Get the substates and subtimesteps used inside the simulator for visualisation
+substates, subtimesteps = Jutul.expand_to_ministeps(result);
 
-substates, subtimesteps = Jutul.expand_to_ministeps(result)
 # We plot primary variables at the outlet through time
 outlet_cell = ncells
-f_outlet = Mocca.plot_cell(substates,model, subtimesteps,outlet_cell)
+f_outlet = Mocca.plot_cell(substates, model, subtimesteps, outlet_cell)
 
-# We plot primary variables along the column at the end of the simulation
-# f_column = Mocca.plot_state(states[end], model)
+# We also plot primary variables along the column at the end of the simulation
+f_column = Mocca.plot_state(substates[end], model)
--- examples/simulate_cyclic.jl
+++ examples/simulate_cyclic.jl
@@ -1,6 +1,6 @@
 #=
 # Cyclic Vacuum Swing Adsorption simulation
-This example shows how to setup and run a cyclic vacuum swing adsorption 
+This example shows how to set up and run a cyclic vacuum swing adsorption
 simulation as described in [Haghpanah et al. 2013](http://dx.doi.org/10.1021/ie302658y)
 
 This simulation inolves injection of a two component flue gas (CO2 and N2)
@@ -8,19 +8,13 @@ into a column of Zeolite 13X. The CO2 is preferentially adsorbed onto the zeolit
 Pressure in the column is then reduced to enable desorption of CO2 for collection.
 
 This is a four stage process comprising:
-* Pressurisation: where the rhs of the column is closed and flue gas is injected from
-the lhs, at velocity `v_feed`, to bring the column pressure up to `PH` (Pressure High)
-* Adsorption: Where both ends of the column are open and flue gas is injected from the 
-rhs at velocity `v_feed`. Pressure at the lhs is `PH`
-* Blowdown: where the lhs of the column is closed and the column is evacuated at
-`PI` (Pressure Intermediate) 
-* Evacuation: where the rhs of the column is closed and the column is evacuated from the 
-lhs at `PL` (Pressure Low).
+* Pressurisation: Where the RHS of the column is closed and flue gas is injected from the LHS, at velocity ``v_{feed}``, to bring the column pressure up to ``P_H`` (Pressure High).
+* Adsorption: Where both ends of the column are open and flue gas is injected from the RHS at velocity ``v_{feed}``. Pressure at the LHS is ``P_H``.
+* Blowdown: Where the LHS of the column is closed and the column is evacuated at ``P_I`` (Pressure Intermediate).
+* Evacuation: Where the RHS of the column is closed and the column is evacuated from the LHS at ``P_L`` (Pressure Low).
 
 Each stage is modelled using the same governing equations but with different boundary
-conditions.
-
-Adsorption onto Zeolite 13X is modelled with a dual-site Langumuir adsorption isotherm.
+conditions. Adsorption onto Zeolite 13X is modelled with a dual-site Langmuir adsorption isotherm.
 =#
 
 # First we load the necessary modules
@@ -28,24 +22,22 @@ import Jutul
 import JutulDarcy
 import Mocca
 
-# We define parameters and setup the system and domain [[simulate_DCB]] example.
-
+# We define parameters, and set up the system and domain as in the [Simulate DCB](simulate_DCB.md) example.
 constants = Mocca.HaghpanahConstants{Float64}()
 permeability = Mocca.compute_permeability(constants)
 axial_dispersion = Mocca.calc_dispersion(constants)
 system = Mocca.TwoComponentAdsorptionSystem(; permeability = permeability, dispersion = axial_dispersion, p = constants)
 
 ncells = 200
-
 dx = sqrt(pi*constants.r_in^2)
 mesh = Jutul.CartesianMesh((ncells, 1, 1), (constants.L, dx, dx))
-domain = Mocca.mocca_domain(mesh, system)
+domain = Mocca.mocca_domain(mesh, system);
 
 # # Create the model
-model = Jutul.SimulationModel(domain, system, general_ad = true)
+# Now we can assemble the model which contains the domain and the system of equations.
+model = Jutul.SimulationModel(domain, system; general_ad = true);
 
-# # Setup the initial state
-
+# # Set up the initial state
 bar = Jutul.si_unit(:bar)
 P_init = 1*bar
 T_init = 298.15
@@ -54,25 +46,25 @@ Tw_init = constants.T_a
 yCO2 = fill(1e-10, ncells)
 y_init = hcat(yCO2, 1 .- yCO2)
 
-state0, prm = Mocca.initialise_state_AdsorptionColumn(P_init, T_init, Tw_init, y_init, model)
+state0, prm = Mocca.initialise_state_AdsorptionColumn(P_init, T_init, Tw_init, y_init, model);
 
-# # Setup the stage timings
+# # Set up the stage timings
 
-# Here we have 4 stages so we specify a length in seconds that we will run each stage. 
+# Here we have 4 stages and we specify a duration in seconds that we will run each stage.
 
 t_press = 15
 t_ads = 15
 t_blow = 30
 t_evac= 40
 
-t_stage = [t_press, t_ads, t_blow, t_evac]
+t_stage = [t_press, t_ads, t_blow, t_evac];
 
 # We also calculate the time taken to run one cycle and the total time at the end of each stage.
 
 cycle_time = sum(t_stage)
-step_end = cumsum(t_stage)
+step_end = cumsum(t_stage);
 
-## Setup boundary conditions
+# # Set up boundary conditions
 
 # We use different boundary conditions for each stage as described above.
 # These boundary conditions are already programmed in Mocca. We just need to define their parameters.
@@ -82,22 +74,23 @@ d_press = Mocca.PressurisationBC(y_feed = constants.y_feed, PH = constants.p_hig
                                 cycle_time = cycle_time, previous_step_end = 0)
 
 d_ads = Mocca.AdsorptionBC(y_feed = constants.y_feed, PH = constants.p_high, v_feed = constants.v_feed,
-                                T_feed = constants.T_feed, cell_left = 1, cell_right = ncells)                          
+                                T_feed = constants.T_feed, cell_left = 1, cell_right = ncells)
 
 d_blow = Mocca.BlowdownBC(PH = constants.p_high, PI = constants.p_intermediate,
                             λ = constants.λ, cell_left = 1, cell_right = ncells,
-                            cycle_time = cycle_time, previous_step_end = step_end[2]) 
+                            cycle_time = cycle_time, previous_step_end = step_end[2])
 
 
 d_evac = Mocca.EvacuationBC(PL = constants.p_low, PI = constants.p_intermediate,
                             λ = constants.λ, cell_left = 1, cell_right = ncells,
-                            cycle_time = cycle_time, previous_step_end = step_end[3])
+                            cycle_time = cycle_time, previous_step_end = step_end[3]);
 
 
-# Now we add them all to the bcs in the sequence that we want to use them.
-bcs = [d_press, d_ads, d_blow, d_evac]
+# We collect the boundary conditions in the order of their associated stages
+bcs = [d_press, d_ads, d_blow, d_evac];
 
-## Specify tim
+# Define the full cyclic simulation by stacking subsequent stages in time
+# for a specified number of cycles
 numcycles = 3
 
 timesteps = []
@@ -107,31 +100,24 @@ maxdt = 1
 for j = 1:numcycles
     for i in eachindex(t_stage)
         numsteps = t_stage[i] / maxdt
-        append!(timesteps,repeat([maxdt],Int(floor(numsteps))))
-        append!(sim_forces,repeat([Jutul.setup_forces(model,bc=bcs[i])],Int(floor(numsteps))))
+        append!(timesteps, repeat([maxdt], Int(floor(numsteps))))
+        append!(sim_forces, repeat([Jutul.setup_forces(model, bc=bcs[i])], Int(floor(numsteps))))
     end
 end
 
-
-
 # # Simulate
-#WRITE 
-states, report = Jutul.simulate(
-    state0,
-    model,
-    timesteps,
+# Now we are ready to run the simulation.
+states, report = Jutul.simulate(state0, model, timesteps;
     forces=sim_forces,
     parameters = prm,
-    info_level = 0
-)
+    info_level = -1
+);
 
 
 # # Plot
-#WRITE : 
-
+# We plot primary variables at the outlet through time
 outlet_cell = ncells
-f_outlet = Mocca.plot_cell(states,model,timesteps,outlet_cell)
-display(f_outlet)
+f_outlet = Mocca.plot_cell(states, model, timesteps, outlet_cell)
 
+# We also plot primary variables along the column at the end of the simulation
 f_column = Mocca.plot_state(states[end], model)
-display(f_column)
