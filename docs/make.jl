fc69707991e2a9686adc530f64161a70e3b99a54
Jakob Torben <Jakob.Torben@sintef.no>
2026-04-10 14:18:22 +0200

    Don't execute notebooks for doc build

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Mocca
 using Literate
 
 
-function build_mocca_docs(; build_examples = true)
+function build_mocca_docs(; build_examples = true, build_notebooks = true)
 
     function update_footer(content, pth)
         return content*"\n\n # ## Example on GitHub\n "*
@@ -34,7 +34,9 @@ function build_mocca_docs(; build_examples = true)
         if build_examples
             upd(content) = update_footer(content, pth)
             Literate.markdown(in_pth, out_dir, preprocess = upd, flavor = Literate.DocumenterFlavor())
-            Literate.notebook(in_pth, out_dir_notebooks, preprocess = upd, flavor = Literate.DocumenterFlavor())
+            if build_notebooks
+                Literate.notebook(in_pth, out_dir_notebooks; preprocess = upd, flavor = Literate.DocumenterFlavor(), execute = false)
+            end
         end
     end
 
@@ -72,4 +74,4 @@ function build_mocca_docs(; build_examples = true)
     )
 end
 
-build_mocca_docs(build_examples=true)
+build_mocca_docs(build_examples = true, build_notebooks = true)
